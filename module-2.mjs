b14878f1f84d436f773d28220179088101875ce7
Rodney St Cloud <hoyrod1@aol.com>
2024-05-16 17:39:35 -0400

    Removed calling the dogApi async function from module-3.mjs to module-2.mjs

--- module-2.mjs
+++ module-2.mjs
@@ -1,4 +1,19 @@
 console.log(
   "==================================== module.mjs 2 ===================================="
 );
+//======================================================================================//
+const dogSelector = document.getElementById("dogBreed");
+//======================================================================================//
 import * as dogBreeds from "./module-3.mjs";
+// console.log(dogBreeds);
+dogBreeds.dogApi().then((dogData) => {
+  // console.log(dogData);
+  const dogDataArray = dogData.data;
+  dogDataArray.forEach((dogElement) => {
+    // console.log(dogElement);
+    let dogOptions = document.createElement("option");
+    dogOptions.setAttribute("value", `${dogElement.id}`);
+    dogOptions.textContent = `${dogElement.name}`;
+    dogSelector.appendChild(dogOptions);
+  });
+});
--- module-3.mjs
+++ module-3.mjs
@@ -2,7 +2,6 @@ console.log(
   "==================================== module.mjs 3 ===================================="
 );
 //======================================================================================//
-const dogSelector = document.getElementById("dogBreed");
 const apiDogKey =
   "live_V3DCshCl0LBcbkwZQT83gsYGPqNLtnfM3Z2UmCDmO5EV4HRD9382dQ3lTLOuOpfr";
 axios.defaults.baseURL = "https://api.thedogapi.com/v1";
@@ -13,14 +12,3 @@ export async function dogApi() {
   const dogsData = await axios("/breeds");
   return dogsData;
 }
-dogApi().then((dogData) => {
-  // console.log(dogData);
-  const dogDataArray = dogData.data;
-  dogDataArray.forEach((dogElement) => {
-    // console.log(dogElement);
-    let dogOptions = document.createElement("option");
-    dogOptions.setAttribute("value", `${dogElement.id}`);
-    dogOptions.textContent = `${dogElement.name}`;
-    dogSelector.appendChild(dogOptions);
-  });
-});
